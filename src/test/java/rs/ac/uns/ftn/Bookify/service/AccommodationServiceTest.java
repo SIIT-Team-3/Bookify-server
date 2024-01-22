46e34f5dc0336048ca3c73e585a03fa4a09657e7
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2024-01-22 19:51:19 +0100

    done service layer testing

--- src/test/java/rs/ac/uns/ftn/Bookify/service/AccommodationServiceTest.java
+++ src/test/java/rs/ac/uns/ftn/Bookify/service/AccommodationServiceTest.java
@@ -4,24 +4,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import rs.ac.uns.ftn.Bookify.enumerations.Status;
 import rs.ac.uns.ftn.Bookify.model.Accommodation;
+import rs.ac.uns.ftn.Bookify.model.Availability;
 import rs.ac.uns.ftn.Bookify.model.Guest;
 import rs.ac.uns.ftn.Bookify.model.Reservation;
 import rs.ac.uns.ftn.Bookify.repository.interfaces.IAccommodationRepository;
 import rs.ac.uns.ftn.Bookify.repository.interfaces.IAvailabilityRepository;
 import rs.ac.uns.ftn.Bookify.repository.interfaces.IPriceListItemRepository;
+import rs.ac.uns.ftn.Bookify.repository.interfaces.IReservationRepository;
 import rs.ac.uns.ftn.Bookify.service.interfaces.IImageService;
 import rs.ac.uns.ftn.Bookify.service.interfaces.INotificationService;
 import rs.ac.uns.ftn.Bookify.service.interfaces.IReservationService;
 import rs.ac.uns.ftn.Bookify.service.interfaces.IUserService;
 
 import java.time.LocalDate;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Stream;
 
 @ExtendWith(MockitoExtension.class)
 public class AccommodationServiceTest {
@@ -29,6 +33,8 @@ public class AccommodationServiceTest {
     private IAccommodationRepository accommodationRepository;
     @Mock
     private IReservationService reservationService;
+    @Mock
+    private IReservationRepository reservationRepository;
     @Mock
     private IUserService userService;
     @Mock
@@ -43,31 +49,14 @@ public class AccommodationServiceTest {
     private AccommodationService accommodationService;
 
     @Test
-    @DisplayName("")
-    public void test_acceptReservationForAccommodation(){
+    @DisplayName("Reservation should not get accepted")
+    public void test_acceptReservationIfAutomaticConformation_manuel() {
         Accommodation accommodation = new Accommodation();
         accommodation.setId(1L);
         accommodation.setManual(true);
-        Reservation reservation = new Reservation(1L, LocalDate.now(), LocalDate.now().plusDays(5), LocalDate.now().plusDays(10),2,300, new Guest(),accommodation, Status.PENDING);
-        Optional<Reservation> r = Optional.of(reservation);
-
-        accommodationService.acceptReservationForAccommodation(reservation);
-
-        Assertions.assertEquals(reservation.getStatus(), Status.ACCEPTED);
-    }
-
-
-    @Test
-    @DisplayName("Reservation should not be accepted")
-    public void test_acceptReservationIfAutomaticConformation_manuel(){
-        Accommodation accommodation = new Accommodation();
-        accommodation.setId(1L);
-        accommodation.setManual(true);
-        Reservation reservation = new Reservation(1L, LocalDate.now(), LocalDate.now().plusDays(5), LocalDate.now().plusDays(10),2,300, new Guest(),accommodation, Status.PENDING);
+        Reservation reservation = new Reservation(1L, LocalDate.now(), LocalDate.now().plusDays(5), LocalDate.now().plusDays(10), 2, 300, new Guest(), accommodation, Status.PENDING);
 
         accommodationService.acceptReservationIfAutomaticConformation(reservation);
-
-        Assertions.assertEquals(reservation.getStatus(), Status.PENDING);
 
         Mockito.verifyNoInteractions(reservationService);
         Mockito.verifyNoInteractions(accommodationRepository);
@@ -76,22 +65,102 @@ public class AccommodationServiceTest {
     }
 
     @Test
-    @DisplayName("")
-    public void test_acceptReservationIfAutomaticConformation_automatic(){
+    @DisplayName("Reservation should proceed to get accepted")
+    public void test_acceptReservationIfAutomaticConformation_automatic() {
         Accommodation accommodation = new Accommodation();
         accommodation.setId(1L);
         accommodation.setManual(false);
-        Reservation reservation = new Reservation(1L, LocalDate.now(), LocalDate.now().plusDays(5), LocalDate.now().plusDays(10),2,300, new Guest(),accommodation, Status.PENDING);
-        Optional<Reservation> r = Optional.of(reservation);
+        Reservation reservation = new Reservation(1L, LocalDate.now(), LocalDate.now().plusDays(5), LocalDate.now().plusDays(10), 2, 300, new Guest(), accommodation, Status.PENDING);
+
         accommodationService.acceptReservationIfAutomaticConformation(reservation);
 
-        Assertions.assertEquals(reservation.getStatus(), Status.ACCEPTED);
-
         Mockito.verify(reservationService).accept(reservation.getId());
+        Mockito.verify(reservationService).rejectOverlappingReservations(reservation.getAccommodation().getId(), reservation.getStart(), reservation.getEnd());
+        Mockito.verifyNoMoreInteractions(reservationService);
         Mockito.verify(accommodationRepository).getReferenceById(accommodation.getId());
         Mockito.verifyNoInteractions(availabilityRepository);
+        Mockito.verify(notificationService).createNotificationGuestRequestResponse(reservation);
+        Mockito.verifyNoMoreInteractions(notificationService);
     }
 
-    //trimOverlapingAvailabilityIntervals
+    @ParameterizedTest
+    @MethodSource(value = "overlappingTestData")
+    @DisplayName("Reservation should get accepted and accommodation's availability shoud get shirnked")
+    public void test_acceptReservationForAccommodation_trimOverlapingAvailabilityIntervals(
+            LocalDate startReservation,
+            LocalDate endReservation,
+            List<Availability> accommodationAvailabilitiesBefore,
+            List<Availability> accommodationAvailabilitiesAfter
+    ) {
+        Accommodation accommodation = new Accommodation();
+        accommodation.setId(1L);
+        accommodation.setManual(false);
+        accommodation.setAvailability(accommodationAvailabilitiesBefore);
+        Reservation reservation = new Reservation();
+        reservation.setStart(startReservation);
+        reservation.setEnd(endReservation);
+        reservation.setAccommodation(accommodation);
 
+        Mockito.when(accommodationRepository.getReferenceById(accommodation.getId())).thenReturn(accommodation);
+        Mockito.when(accommodationRepository.getAvailabilityItemsOverlapsWith(accommodation.getId(), reservation.getStart(), reservation.getEnd())).thenReturn(new ArrayList<Availability>(accommodationAvailabilitiesBefore));
+
+        accommodationService.acceptReservationIfAutomaticConformation(reservation);
+        Assertions.assertEquals(accommodationAvailabilitiesAfter.size(), accommodation.getAvailability().size());
+        for (Availability a : accommodationAvailabilitiesAfter) {
+            Assertions.assertTrue(() -> {
+                for(Availability availability :accommodation.getAvailability()){
+                    if(availability.getStartDate().equals(a.getStartDate()) && availability.getEndDate().equals(a.getEndDate())) return true;
+                }
+                return false;
+            });
+        }
+
+        Mockito.verify(reservationService).accept(reservation.getId());
+        Mockito.verify(reservationService).rejectOverlappingReservations(reservation.getAccommodation().getId(), reservation.getStart(), reservation.getEnd());
+        Mockito.verifyNoMoreInteractions(reservationService);
+        Mockito.verify(accommodationRepository).getReferenceById(accommodation.getId());
+        Mockito.verify(notificationService).createNotificationGuestRequestResponse(reservation);
+        Mockito.verifyNoMoreInteractions(notificationService);
+
+    }
+
+    static Stream<Arguments> overlappingTestData() {
+        return Stream.of(
+                // starts overlapping
+                Arguments.arguments(LocalDate.of(2024, 4, 15), LocalDate.of(2024, 4, 20),
+                        new ArrayList<>(Arrays.asList(new Availability(1L, LocalDate.of(2024, 4, 1), LocalDate.of(2024, 4, 20)))),
+                        new ArrayList<>(Arrays.asList(new Availability(1L, LocalDate.of(2024, 4, 1), LocalDate.of(2024, 4, 14))))),
+
+                // endings overlapping
+                Arguments.arguments(LocalDate.of(2024, 4, 20), LocalDate.of(2024, 5, 1),
+                        new ArrayList<>(Arrays.asList(new Availability(1L, LocalDate.of(2024, 4, 20), LocalDate.of(2024, 5, 5)))),
+                        new ArrayList<>(Arrays.asList(new Availability(1L, LocalDate.of(2024, 5, 2), LocalDate.of(2024, 5, 5))))),
+
+                // containing
+                Arguments.arguments(LocalDate.of(2024, 6, 1), LocalDate.of(2024, 6, 10),
+                        new ArrayList<>(Arrays.asList(new Availability(1L, LocalDate.of(2024, 5, 20), LocalDate.of(2024, 6, 15)))),
+                        new ArrayList<>(Arrays.asList(
+                                new Availability(1L, LocalDate.of(2024, 5, 20), LocalDate.of(2024, 5, 31)),
+                                new Availability(2L, LocalDate.of(2024, 6, 11), LocalDate.of(2024, 6, 15)))
+                        )),
+
+                // start and ending overlapping
+                Arguments.arguments(LocalDate.of(2024, 4, 15), LocalDate.of(2024, 4, 20),
+                        new ArrayList<>(Arrays.asList(new Availability(1L, LocalDate.of(2024, 4, 15), LocalDate.of(2024, 4, 20)))),
+                        new ArrayList<>())
+
+                // overlapping with two availabilties - not possible because of merge
+//                Arguments.arguments(LocalDate.of(2024, 6, 10), LocalDate.of(2024, 6, 20),
+//                        new ArrayList<>(Arrays.asList(
+//                                new Availability(1L, LocalDate.of(2024, 5, 20), LocalDate.of(2024, 6, 15)),
+//                                new Availability(2L, LocalDate.of(2024, 6, 16), LocalDate.of(2024, 6, 30))
+//                        )),
+//                        new ArrayList<>(Arrays.asList(
+//                                new Availability(1L, LocalDate.of(2024, 5, 20), LocalDate.of(2024, 6, 9)),
+//                                new Availability(2L, LocalDate.of(2024, 5, 20), LocalDate.of(2024, 5, 31)),
+//                                new Availability(3L, LocalDate.of(2024, 5, 20), LocalDate.of(2024, 5, 31)),
+//                                new Availability(4L, LocalDate.of(2024, 6, 11), LocalDate.of(2024, 6, 15))
+//                        )))
+                );
+    }
 }
--- src/test/java/rs/ac/uns/ftn/Bookify/service/ReservationServiceTest.java
+++ src/test/java/rs/ac/uns/ftn/Bookify/service/ReservationServiceTest.java
@@ -5,19 +5,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import rs.ac.uns.ftn.Bookify.enumerations.Status;
 import rs.ac.uns.ftn.Bookify.exception.BadRequestException;
 import rs.ac.uns.ftn.Bookify.model.Accommodation;
+import rs.ac.uns.ftn.Bookify.model.Availability;
 import rs.ac.uns.ftn.Bookify.model.Guest;
 import rs.ac.uns.ftn.Bookify.model.Reservation;
 import rs.ac.uns.ftn.Bookify.repository.interfaces.IReservationRepository;
 import rs.ac.uns.ftn.Bookify.service.interfaces.IAccommodationService;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @ExtendWith(MockitoExtension.class)
 public class ReservationServiceTest {
@@ -148,7 +155,7 @@ public class ReservationServiceTest {
         Mockito.verifyNoMoreInteractions(reservationRepository);
     }
     @Test
-    @DisplayName("Should return bad request exception response date expired")
+    @DisplayName("Should return bad request exception reservation not found")
     public void test_reject_fail_bad_request_reservation_not_found(){
         Mockito.when(reservationRepository.findById(1L)).thenReturn(Optional.empty());
         Throwable t = Assertions.assertThrows(BadRequestException.class, () -> reservationService.reject(1L));
@@ -158,37 +165,45 @@ public class ReservationServiceTest {
         Mockito.verify(reservationRepository, Mockito.atMostOnce()).findById(1L);
         Mockito.verifyNoMoreInteractions(reservationRepository);
     }
-    // Get guest reservations
-    @Test
-    @DisplayName("Should return all guest accepted requests (Reservations)")
-    public void test_getReservationsByGuestId(){
-        Guest guest = new Guest();
-        guest.setId(1L);
-
-    }
-
-    // Accept automatic
-
-    @Test
-    @DisplayName("Should accept reservation after creating it")
-    public void test_insert_automatic_accepting_requests(){
-        Accommodation accommodation = new Accommodation();
-        accommodation.setId(1L);
-        accommodation.setManual(false);
-        Reservation reservation = new Reservation(1L, LocalDate.now(), LocalDate.now().plusDays(5), LocalDate.now().plusDays(10),2,300, new Guest(), new Accommodation(), Status.PENDING);
-        Optional<Reservation> r = Optional.of(reservation);
-
-
-
-
-    }
 
     // rejectOverlappingReservations
-    @Test
+    @ParameterizedTest
+    @MethodSource(value = "getReservationsForRejecting")
     @DisplayName("No overlaping reservations")
-    public void test_rejectOverlappingReservations(){
+    public void test_rejectOverlappingReservations(ArrayList<Reservation> reservationsToCancel){
+        Reservation reservation = new Reservation();
+        Accommodation accommodation = new Accommodation();
+        accommodation.setId(1L);
+        reservation.setStart(LocalDate.now());
+        reservation.setEnd(LocalDate.now().plusDays(2));
+        reservation.setAccommodation(accommodation);
+        Mockito.when(reservationRepository.findReservationsByAccommodation_IdAndStartBeforeAndEndAfterAndStatusNotIn(
+                reservation.getAccommodation().getId(),
+                reservation.getStart(), reservation.getEnd(),
+                EnumSet.of(Status.CANCELED, Status.ACCEPTED, Status.REJECTED)
+        )).thenReturn(reservationsToCancel);
 
+        reservationService.rejectOverlappingReservations(reservation.getAccommodation().getId(), reservation.getEnd(), reservation.getStart());
+
+        reservationsToCancel.forEach(reservation1 -> {
+            Assertions.assertEquals(Status.REJECTED, reservation1.getStatus());
+            Mockito.verify(reservationRepository).save(reservation1);
+        });
+
+        Mockito.verify(reservationRepository).findReservationsByAccommodation_IdAndStartBeforeAndEndAfterAndStatusNotIn(reservation.getAccommodation().getId(),
+                reservation.getStart(), reservation.getEnd(),
+                EnumSet.of(Status.CANCELED, Status.ACCEPTED, Status.REJECTED));
+        Mockito.verifyNoMoreInteractions(reservationRepository);
     }
-    // save
-
+    static Stream<Arguments> getReservationsForRejecting() {
+        return Stream.of(
+                // starts overlapping
+                Arguments.arguments(new ArrayList<>(Arrays.asList(
+                        new Reservation(1L, LocalDate.now().minusDays(2), LocalDate.now(), LocalDate.now().plusDays(3), 2, 300, new Guest(), new Accommodation(), Status.PENDING),
+                        new Reservation(2L, LocalDate.now().minusDays(3), LocalDate.now(), LocalDate.now().plusDays(4), 2, 300, new Guest(), new Accommodation(), Status.PENDING),
+                        new Reservation(3L, LocalDate.now().minusDays(4), LocalDate.now(), LocalDate.now().plusDays(5), 2, 300, new Guest(), new Accommodation(), Status.PENDING),
+                        new Reservation(4L, LocalDate.now().minusDays(5), LocalDate.now(), LocalDate.now().plusDays(6), 2, 300, new Guest(), new Accommodation(), Status.PENDING)
+                        )))
+        );
+    }
 }
